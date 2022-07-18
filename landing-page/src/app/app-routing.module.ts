8cb48f780dfae7b9a3478d7fc0343347f8ce9a53
Axel Moncada <tatomoncada@gmail.com>
2022-07-17 19:04:56 -0500

    Challenge terminado

--- landing-page/src/app/app-routing.module.ts
+++ landing-page/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { IndexComponent } from './paginas/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:IndexComponent},
+  {path: 'contacto', component:ContactoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- landing-page/src/app/app.module.ts
+++ landing-page/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './paginas/index/index.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    MenuComponent,
+    FooterComponent,
+    ContactoComponent,
+    
   ],
   imports: [
     BrowserModule,
